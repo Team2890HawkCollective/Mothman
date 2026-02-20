5adb6549b5d921d634999d98533099ca7f9109f8
Mehooliu <matthewchen2424@gmail.com>
2026-02-20 16:20:26 -0500

    first commit

---
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -0,0 +1,44 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class ClimberSubsystem extends SubsystemBase{
+    private static TalonFX climberMotor = new TalonFX(Constants.ClimberConstants.CLIMB_MOTOR_ID);
+    private static Servo climberRatchet = new Servo(Constants.ClimberConstants.RATCHET_PWM_PORT);
+
+    public void liftRobot() {
+        climberMotor.set(Constants.ClimberConstants.CLIMBER_SPEED);
+    }
+
+    public void lowerRobot() {
+        climberMotor.set(Constants.ClimberConstants.CLIMBER_SPEED * -1);
+    }
+
+    public void stopClimber() {
+        climberMotor.set(0);
+    }
+
+    public Command liftRobotCommand() {
+        return runOnce(() -> toggleRatchet(true)).andThen(() -> liftRobot());
+    }
+
+    public Command lowerRobotCommand() {
+        return runOnce(() -> toggleRatchet(false)).andThen(() -> lowerRobot());
+    }
+
+    public Command stopClimberCommand() {
+        return runOnce(() -> stopClimber());
+    }
+
+    public static void toggleRatchet(boolean toggle) {
+    if (toggle == true) {
+      climberRatchet.set(Constants.ClimberConstants.RATCHET_LOCK_ANGLE);
+    } else
+      climberRatchet.set(Constants.ClimberConstants.RATCHET_UNLOCK_ANGLE);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/TargetingSubsystems.java
@@ -0,0 +1,178 @@
+package frc.robot.subsystems;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import com.pathplanner.lib.path.GoalEndState;
+import com.pathplanner.lib.path.PathConstraints;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.path.PathPoint;
+import com.pathplanner.lib.path.RotationTarget;
+import com.pathplanner.lib.path.Waypoint;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.robot.Constants;
+import frc.robot.LimelightHelpers;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardComponent;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.RobotContainer;
+import frc.robot.Constants;
+
+public class TargetingSubsystems extends SubsystemBase {
+
+    RobotContainer m_RobotContainer = new RobotContainer();
+
+    PhotonCamera photonVision = new PhotonCamera("Arducam_OV9281_USB_Camera");
+    Transform3d BACK_LEFT_CAMERA_OFFSETS = new Transform3d(new Translation3d(0, 0, 0), new Rotation3d(0, 0, 0));
+    PhotonPoseEstimator photonEstimator = new PhotonPoseEstimator(
+            AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltAndymark),
+            BACK_LEFT_CAMERA_OFFSETS);
+    PIDController photonAimPIDController = new PIDController(0.3, 0, 0.001);
+
+    public TargetingSubsystems() {
+        photonAimPIDController.enableContinuousInput(-180, 180);
+    }
+
+    Pose2d currentRobotPose;
+
+    public List<Waypoint> rightClimbWaypoints;
+
+    public Command pathPlanToRightClimbPoseCommand() {
+        GoalEndState goalEndState = new GoalEndState(0, Constants.TargetingConstants.RIGHT_CLIMB_POSE.getRotation());
+        PathConstraints goToClimbConstraints = new PathConstraints(3.0, 3.0, 3.0, 6.0, 12.0);
+        currentRobotPose = m_RobotContainer.getSwerveDriveBase().getPose();
+        rightClimbWaypoints = PathPlannerPath.waypointsFromPoses(
+                currentRobotPose, Constants.TargetingConstants.RIGHT_CLIMB_POSE);
+
+        PathPlannerPath goToClimbPath = new PathPlannerPath(rightClimbWaypoints, goToClimbConstraints, null,
+                goalEndState);
+        goToClimbPath.preventFlipping = true;
+
+        return m_RobotContainer.getSwerveDriveBase().getAutonomousCommand("goToClimbPath");
+    }
+
+    public Command aimAndRangeToPose(Pose2d desiredPose) {
+        return new RunCommand(() -> {
+         currentRobotPose = m_RobotContainer.getSwerveDriveBase().getPose();
+
+            Transform2d errorFromDesiredPose = desiredPose.minus(currentRobotPose);
+
+            double xError = errorFromDesiredPose.getX();
+            double yError = errorFromDesiredPose.getY();
+            double angleError = errorFromDesiredPose.getRotation().getRadians();
+
+            PIDController xController = new PIDController(1.5, 0, 0);
+            PIDController yController = new PIDController(1.5, 0, 0);
+            PIDController angleController = new PIDController(3.0, 0, 0);
+
+            angleController.enableContinuousInput(-Math.PI, Math.PI);
+
+            double xSpeed = xController.calculate(currentRobotPose.getX(), desiredPose.getX());
+            double ySpeed = yController.calculate(currentRobotPose.getY(), desiredPose.getY());
+            double angleSpeed = angleController.calculate(currentRobotPose.getRotation().getRadians(),
+                    desiredPose.getRotation().getRadians());
+            
+            m_RobotContainer.getSwerveDriveBase().drive(new Translation2d(xSpeed, ySpeed), angleSpeed, true);
+        }, m_RobotContainer.getSwerveDriveBase());
+    }
+
+    Command photonAimAtClimb = new RunCommand(() -> {
+        double rot = 0.0;
+        var result = photonVision.getLatestResult();
+        if (result.hasTargets()) {
+            double yawError = result.getBestTarget().getYaw();
+            rot = photonAimPIDController.calculate(yawError, 0);
+        }
+
+        rot = MathUtil.clamp(rot, -3.0, 3.0);
+
+        m_RobotContainer.getSwerveDriveBase().drive(new Translation2d(m_RobotContainer.getDriverXbox().getLeftY() * -1,
+                m_RobotContainer.getDriverXbox().getLeftX() * -1), rot, true);
+    }, m_RobotContainer.getSwerveDriveBase());
+
+
+    public PhotonPoseEstimator getPhotonPoseEstimator() {
+        return photonEstimator;
+    }
+
+    // static public NetworkTable table =
+    // NetworkTableInstance.getDefault().getTable(Constants.LimeLight.LIMELIGHT_NAME);
+    // static public NetworkTableEntry ty = table.getEntry("ty");
+    // static double targetOffsetAngle_Vertical = ty.getDouble(0.0);
+
+    // how many degrees back is your limelight rotated from perfectly vertical?
+    static double limelightMountAngleDegrees = 25.0;
+
+    // distance from the center of the Limelight lens to the floor
+    static double limelightLensHeightInches = 27.5;
+
+    // distance from the target to the floor
+    static double goalHeightInches = 44;
+
+    static double angleToGoalDegrees = limelightMountAngleDegrees + Constants.LimeLight.LIMELIGHT_TY;
+    static double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
+
+    // calculate distance
+    static double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches)
+            / Math.tan(angleToGoalRadians);
+
+    public static double getDistanceFromAprilTag() {
+        angleToGoalDegrees = limelightMountAngleDegrees + Constants.LimeLight.LIMELIGHT_TY;
+        angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
+        distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches)
+                / Math.tan(angleToGoalRadians);
+        return distanceFromLimelightToGoalInches;
+    }
+
+    @Override
+    public void periodic() {
+
+        Optional<EstimatedRobotPose> result = photonEstimator.update(photonVision.getLatestResult());
+
+        if (result.isPresent()) {
+            EstimatedRobotPose estimatedPose = result.get();
+            m_RobotContainer.getSwerveDriveBase().getSwerveDrive()
+                    .addVisionMeasurement(estimatedPose.estimatedPose.toPose2d(), estimatedPose.timestampSeconds);
+        }
+        /*
+         * Shuffleboard.getTab("Vision").add("Photon Vision Yaw Value",
+         * photonVision.getLatestResult().getBestTarget().getYaw());
+         * Shuffleboard.getTab("Vision").add("Photon Vision Pitch Value",
+         * photonVision.getLatestResult().getBestTarget().getPitch());
+         * Shuffleboard.getTab("Vision").add("Limelight TX Value",
+         * LimelightHelpers.getTX("limelight"));
+         * Shuffleboard.getTab("Vision").add("Limelight April Tag ID",
+         * LimelightHelpers.getFiducialID("limelight"));
+         * Shuffleboard.getTab("Vision").addCamera("Limelight", "limelight", null);
+         * Shuffleboard.getTab("Vision").addCamera("Photon",
+         * "Arducam_OV9281_USB_Camera",
+         * "http://photonvision.local:5800");
+         */
+    }
+}
